3a3b005e67dac2bd9a64b51c1d753cd42df3d045
Cameron <walk1524@umn.edu>
2026-03-17 14:18:38 -0500

    Fixed client side errors

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { OperatorDashComponent } from './operator-dash/operator-dash.component';
 import { FamilyListComponent } from './family/family-list.component';
 import { AddFamilyComponent } from './family/add-family.component';
-import { InventoryListComponent } from './inventory/inventory-list.component';
-import { AddInventoryComponent } from './inventory/add-inventory.component';
+import { InventoryComponent } from 'src/inventory/inventory.component';
+// import { InventoryListComponent } from './inventory/inventory-list.component';
+
+// import { AddInventoryComponent } from './inventory/add-inventory.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'dashboard', component: OperatorDashComponent, title: 'Operator Dashboard'},
   {path: 'family', component: FamilyListComponent, title: 'Family'},
   {path: 'family/new', component: AddFamilyComponent, title: 'Add Family'},
-  {path: 'inventory', component: InventoryListComponent, title: 'Inventory'},
-  {path: 'inventory/new', component: AddInventoryComponent, title: 'Add Inventory'}
+  {path: 'inventory', component: InventoryComponent, title: 'Inventory'}
+  // {path: 'inventory/new', component: AddInventoryComponent, title: 'Add Inventory'}
 ];
 
 @NgModule({
